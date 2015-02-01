7551a540a3c971afa03a4228d63f5e24309f506b
Björn Westlin <bwestlin@gmail.com>
2015-02-01 19:23:08 +0100

    Created AnimationHelper and added fps counter

--- js/canvas-helpers.js
+++ js/canvas-helpers.js
@@ -8,3 +8,42 @@ window.requestAnimFrame = (function(callback) {
            window.setTimeout(callback, 1000 / 60);
          };
 })();
+
+var AnimationHelper = (function () {
+  var callbacks = [];
+  var animating = false;
+  var lastTime = null;
+
+  function registerAnimCallback(callback) {
+    callbacks.push(callback);
+  }
+
+  function startAnim() {
+    animating = true;
+    startTime = null;
+    requestAnimFrame(doAnimation);
+  }
+
+  function stopAnim() {
+    animating = false;
+  }
+
+  function doAnimation(time) {
+    var lastFrameMillisec = undefined;
+    if (time) {
+      if (lastTime) lastFrameMillisec = time - lastTime;
+      lastTime = time;
+    }
+    for (var i = 0; i < callbacks.length; i++) callbacks[i](lastFrameMillisec);
+
+    if (animating) {
+      requestAnimFrame(doAnimation);
+    }
+  }
+
+  return {
+    registerAnimCallback: registerAnimCallback,
+    startAnim: startAnim,
+    stopAnim: stopAnim
+  };
+})();
